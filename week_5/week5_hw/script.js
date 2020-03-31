6d0907cd91d249b94997ac2af35965381ce48340
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-03-30 23:46:51 -0700

    Week5HW-Style
    Edited colors, typeface, spacing, margins.
    Added Jquery to add classes to cart to move it to the right when the button is pressed.

--- week_5/week5_hw/script.js
+++ week_5/week5_hw/script.js
@@ -1,70 +1,73 @@
   $(document).ready(function () {
-    //   FORM VALIDATION
-    $("form.needs-validation").submit(function(event){
-        event.preventDefault();
-  
+      //   FORM VALIDATION
+      $("form.needs-validation").submit(function (event) {
+          event.preventDefault();
+
           $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
       })
 
-    // CHECKBOX BILLING SAME
-    $('#check-same').click(function () {
-      if ($('#check-same').is(':checked')) {
-        $('#firstName-billing').val($('#firstName').val());
-        $('#lastName-billing').val($('#lastName').val());
-        $('#inputAddress-billing').val($('#inputAddress').val());
-        $('#inputCity-billing').val($('#inputCity').val());
-        $('#inputZip-billing').val($('#inputZip').val());
-        var state = $('#inputState option:selected').val();
-        $('#inputState-billing option[value=' + state + ']').attr('selected', 'selected');
-      } else {
-        //Clear on uncheck
-        $('#firstName-billing').val("");
-        $('#lastName-billing').val("");
-        $('#inputAddress-billing').val("");
-        $('#inputCity-billing').val("");
-        $('#inputZip-billing').val("");
-        $('#inputState-billing option[value=""]').attr('selected', 'selected');
-      };
-    });
+      // CHECKBOX BILLING SAME
+      $('#check-same').click(function () {
+          if ($('#check-same').is(':checked')) {
+              $('#firstName-billing').val($('#firstName').val());
+              $('#lastName-billing').val($('#lastName').val());
+              $('#inputAddress-billing').val($('#inputAddress').val());
+              $('#inputCity-billing').val($('#inputCity').val());
+              $('#inputZip-billing').val($('#inputZip').val());
+              var state = $('#inputState option:selected').val();
+              $('#inputState-billing option[value=' + state + ']').attr('selected', 'selected');
+          } else {
+              //Clear on uncheck
+              $('#firstName-billing').val("");
+              $('#lastName-billing').val("");
+              $('#inputAddress-billing').val("");
+              $('#inputCity-billing').val("");
+              $('#inputZip-billing').val("");
+              $('#inputState-billing option[value=""]').attr('selected', 'selected');
+          };
+      });
 
 
-// REMOVE ITEM LIST WITH BUTTON CLICKS
-$( ".proceed" ).click(function() {
-    $( ".cart" ).hide();
+      // ADD CLASS TO CART
+      $(".proceed").click(function () {
+          $(".cart").addClass("col-md-4 order-md-2 float-right");
+      });
+
+      //   SHOW SHIPPING SECTION WITH BUTTON CLICK
+      $(".proceed").click(function () {
+          $("#shipping").show();
+      });
+
+      //   SHOW SHIPPING SECTION WITH BUTTON CLICK
+      $(".proceed").click(function () {
+          $(".proceed").hide();
+      });
+
+      // VALIDATE FORM FIELDSET FOR NEXT FIELDSET TO OPEN
+      $("#shipping input").on("keyup change", function () {
+          var inputValidate = true;
+          $("#shipping input").each(function (index, element) {
+              if ($(element).val() == "")
+                  inputValidate = false;
+          });
+
+          if (inputValidate)
+              $("#billing").show("slow");
+          else
+              $("#billing").hide();
+      });
+
+      $("#billing input").on("keyup change", function () {
+          var inputValidate = true;
+          $("#billing input").each(function (index, element) {
+              if ($(element).val() == "")
+                  inputValidate = false;
+          });
+
+          if (inputValidate)
+              $("#credit").show("slow");
+          else
+              $("#credit").hide();
+      });
+
   });
-
-//   SHOW SHIPPING SECTION WITH BUTTON CLICK
-  $( ".proceed" ).click(function() {
-    $( "#shipping" ).show();
-  });
-
-    // VALIDATE FORM FIELDSET FOR NEXT FIELDSET TO OPEN
-    $("#shipping input").on("keyup change", function(){
-        var inputValidate = true;
-        $("#shipping input").each(function(index, element){   		
-            if ($(element).val() == "")
-               inputValidate = false;
-        });
-        
-        if (inputValidate)
-            $("#billing").show("slow");
-        else
-            $("#billing").hide();
-    });
-
-    $("#billing input").on("keyup change", function(){
-        var inputValidate = true;
-        $("#billing input").each(function(index, element){   		
-            if ($(element).val() == "")
-               inputValidate = false;
-        });
-        
-        if (inputValidate)
-            $("#credit").show("slow");
-        else
-            $("#credit").hide();
-    });
-
-
-
-    });
