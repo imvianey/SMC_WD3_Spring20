60d6156f1c660036171f92b016f7e2c85b5db29b
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-04-05 23:11:00 -0700

    midterm-Image horizontal scroll
    Script:  Added scroll that moves image horizontally across jumbotron.
    
    Added main css: jumbotron, section, moving image
    
    Index:  added moving image into the  jumbotron div

--- midterm/taunt_global/script.js
+++ midterm/taunt_global/script.js
@@ -1,17 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //we write everything here
 
     $('#myCarousel').carousel({
         interval: 330
     });
 
-    $('#carouselControl').click(function(){
-        
-        if( $('#carouselControl').hasClass('paused') ){
+    $('#carouselControl').click(function () {
+
+        if ($('#carouselControl').hasClass('paused')) {
             $('#myCarousel').carousel('cycle');
             $('#carouselControl').text('Pause');
-        }
-        else {
+        } else {
             $('#myCarousel').carousel('pause');
             $('#carouselControl').text('Play');
         }
@@ -20,9 +19,25 @@ $(document).ready(function(){
     });
 
 
-    $('#modalLauncher').click(function(){
+    $('#modalLauncher').click(function () {
         $('#exampleModal').modal({
             backdrop: false
         });
     });
+
+    // SCROLL IMAGE TRAVELS HORIZONTALLY
+    $(window).on("load resize scroll", function () {
+        $(".bg-static").each(function () {
+            var windowTop = $(window).scrollTop();
+            var elementTop = $(this).offset().top;
+            var leftPosition = windowTop - elementTop;
+            $(this)
+                .find(".bg-move")
+                .css({
+                    right: leftPosition
+                });
+        });
+    });
+
+
 });
