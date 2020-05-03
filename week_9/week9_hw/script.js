dc8000653f2b71d3562ecf8fbc98c771f381906b
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-05-03 03:56:43 -0700

    Week9 HW- Jquery rotate hover
    Added rotate to bugs pic with hover random jquery

--- week_9/week9_hw/script.js
+++ week_9/week9_hw/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
 
+    $('.rotate').hover(function() {
+        var a = Math.random() * -40 - 1;
+        $(this).css('transform', 'rotate(' + a + 'deg)');
+    }, function() {
+        $(this).css('transform', 'none');
+    });
 
 });
