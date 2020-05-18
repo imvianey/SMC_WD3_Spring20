2e8e91a3e74a8a4cf0048fb0b8db8de6b885ae54
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-05-18 16:50:58 -0700

    week11 hw- page css
    Background, font (color, style, family, size), border, button, score

--- week_11/week11_hw/canvas.js
+++ week_11/week11_hw/canvas.js
@@ -1,69 +1,82 @@
-  
  var canvas = document.getElementById("myCanvas");
- var context = canvas.getContext("2d");
+  var context = canvas.getContext("2d");
 
- var ballColor = 'rgb(100, 205, 120)';
- var ballPosX = canvas.width/2;
- var ballPosY = canvas.height/2;
- var ballRadius = 50;
+  var ballColor = 'rgb(100, 205, 120)';
+  var ballPosX = canvas.width / 2;
+  var ballPosY = canvas.height / 2;
+  var ballRadius = 50;
 
- var ballSpeedX = 3;
- var ballSpeedY = 2;
+  var ballSpeedX = 3;
+  var ballSpeedY = 2;
 
- var animate = function(){
-   context.clearRect(0, 0, canvas.width, canvas.height);
+  var animate = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = ballColor;
+    context.beginPath();
+    context.arc(ballPosX, ballPosY, ballRadius, 0, 2 * Math.PI);
+    context.fill();
+
+    if ((ballPosX + ballRadius >= canvas.width) || (ballPosX - ballRadius) <= 0) {
+      ballSpeedX = ballSpeedX * -1;
+    }
+
+    if ((ballPosY + ballRadius >= canvas.height) || (ballPosY - ballRadius <= 0)) {
+      ballSpeedY = ballSpeedY * -1;
+    }
+
+    ballPosX = ballPosX + ballSpeedX;
+    ballPosY = ballPosY + ballSpeedY;
 
 
-   context.fillStyle = ballColor;
-   context.beginPath();
-   context.arc(ballPosX, ballPosY, ballRadius, 0, 2*Math.PI);
-   context.fill();
-
-   if((ballPosX + ballRadius >= canvas.width) || (ballPosX - ballRadius) <= 0){
-     ballSpeedX = ballSpeedX * -1;
-   }
-
-   if( ( ballPosY + ballRadius >= canvas.height) || ( ballPosY - ballRadius <= 0) ){
-     ballSpeedY = ballSpeedY * -1;
-   }
-
-   ballPosX = ballPosX + ballSpeedX;
-   ballPosY = ballPosY + ballSpeedY;
-
-
-   window.requestAnimationFrame(animate);
- }
-
- animate();
-
-//  CHANGE BALL SPEED ON CLICK
- canvas.addEventListener("click", function(event){
-   var distX = Math.abs(ballPosX - event.offsetX);
-   var distY = Math.abs(ballPosY - event.offsetY);
-
-   if(distX < ballRadius && distY < ballRadius){
-     ballSpeedX = (Math.floor(Math.random() * 10));
-   }
- })
-
-//  SCORE COUNT
- var button = document.getElementById("scoreCount"),
-  count = 0;
-  canvas.onclick = function() {
-  var distX = Math.abs(ballPosX - event.offsetX);
-  var distY = Math.abs(ballPosY - event.offsetY);
-  if(distX < ballRadius && distY < ballRadius){
-    count += 1;
-    button.innerHTML = "Score: " + count;
+    window.requestAnimationFrame(animate);
   }
-};
+
+  animate();
 
 
-// RANDOM COLOR CHANGE
-const randomColor = () => {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  ballColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
-}
-colorButton.addEventListener("click", randomColor);
-randomColor();
+  //  CHANGE BALL SPEED ON CLICK
+  canvas.addEventListener("click", function (event) {
+    var distX = Math.abs(ballPosX - event.offsetX);
+    var distY = Math.abs(ballPosY - event.offsetY);
+    var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+
+    if (distX < ballRadius && distY < ballRadius) {
+      ballSpeedX = (Math.floor(Math.random() * 10));
+    }
+
+    //  if(distX < ballRadius && distY < ballRadius){
+    //   ballColor = "#" + (Math.floor(Math.random()*16777215).toString(16));
+    // }
+    if (distX < ballRadius && distY < ballRadius) {
+      ballColor = colors[Math.floor(Math.random() * colors.length)];
+    }
+  })
+
+  //  SCORE COUNT
+  var button = document.getElementById("scoreCount"),
+    count = 0;
+  canvas.onclick = function () {
+    var distX = Math.abs(ballPosX - event.offsetX);
+    var distY = Math.abs(ballPosY - event.offsetY);
+    if (distX < ballRadius && distY < ballRadius) {
+      count += 1;
+      button.innerHTML = "SCORE: " + count;
+    }
+  };
+
+
+  // RANDOM COLOR CHANGE
+  // const randomColor = () => {
+  //   const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  //   ballColor = "#" + randomColor;
+  //   color.innerHTML = "#" + randomColor;
+  // }
+  // colorButton.addEventListener("click", randomColor);
+  // randomColor();
+
+
+
+  blackButton.addEventListener("click", function (event) {
+    ballColor = 'black';
+  });
