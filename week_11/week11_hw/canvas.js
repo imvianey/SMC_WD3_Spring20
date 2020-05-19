1e85b946727e76f7c4250df45d6f37aa82f78c77
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-05-18 17:16:38 -0700

    week11 hw- ball color
    Shadow blur on ball.  Changed ball color when clicked.
    
    Beautified files

--- week_11/week11_hw/canvas.js
+++ week_11/week11_hw/canvas.js
@@ -1,82 +1,69 @@
  var canvas = document.getElementById("myCanvas");
-  var context = canvas.getContext("2d");
+ var context = canvas.getContext("2d");
 
-  var ballColor = 'rgb(100, 205, 120)';
-  var ballPosX = canvas.width / 2;
-  var ballPosY = canvas.height / 2;
-  var ballRadius = 50;
+ var ballColor = '#fff';
+ var ballPosX = canvas.width / 2;
+ var ballPosY = canvas.height / 2;
+ var ballRadius = 50;
 
-  var ballSpeedX = 3;
-  var ballSpeedY = 2;
+ var ballSpeedX = 3;
+ var ballSpeedY = 2;
 
-  var animate = function () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+ var animate = function () {
+   context.clearRect(0, 0, canvas.width, canvas.height);
 
-    context.fillStyle = ballColor;
-    context.beginPath();
-    context.arc(ballPosX, ballPosY, ballRadius, 0, 2 * Math.PI);
-    context.fill();
+   context.fillStyle = ballColor;
+   context.beginPath();
+   context.arc(ballPosX, ballPosY, ballRadius, 0, 2 * Math.PI);
+   context.fill();
+   context.shadowBlur = 20;
+   context.shadowColor = "white";
 
-    if ((ballPosX + ballRadius >= canvas.width) || (ballPosX - ballRadius) <= 0) {
-      ballSpeedX = ballSpeedX * -1;
-    }
+   if ((ballPosX + ballRadius >= canvas.width) || (ballPosX - ballRadius) <= 0) {
+     ballSpeedX = ballSpeedX * -1;
+   }
 
-    if ((ballPosY + ballRadius >= canvas.height) || (ballPosY - ballRadius <= 0)) {
-      ballSpeedY = ballSpeedY * -1;
-    }
+   if ((ballPosY + ballRadius >= canvas.height) || (ballPosY - ballRadius <= 0)) {
+     ballSpeedY = ballSpeedY * -1;
+   }
 
-    ballPosX = ballPosX + ballSpeedX;
-    ballPosY = ballPosY + ballSpeedY;
-
-
-    window.requestAnimationFrame(animate);
-  }
-
-  animate();
+   ballPosX = ballPosX + ballSpeedX;
+   ballPosY = ballPosY + ballSpeedY;
 
 
-  //  CHANGE BALL SPEED ON CLICK
-  canvas.addEventListener("click", function (event) {
-    var distX = Math.abs(ballPosX - event.offsetX);
-    var distY = Math.abs(ballPosY - event.offsetY);
-    var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+   window.requestAnimationFrame(animate);
+ }
 
-    if (distX < ballRadius && distY < ballRadius) {
-      ballSpeedX = (Math.floor(Math.random() * 10));
-    }
-
-    //  if(distX < ballRadius && distY < ballRadius){
-    //   ballColor = "#" + (Math.floor(Math.random()*16777215).toString(16));
-    // }
-    if (distX < ballRadius && distY < ballRadius) {
-      ballColor = colors[Math.floor(Math.random() * colors.length)];
-    }
-  })
-
-  //  SCORE COUNT
-  var button = document.getElementById("scoreCount"),
-    count = 0;
-  canvas.onclick = function () {
-    var distX = Math.abs(ballPosX - event.offsetX);
-    var distY = Math.abs(ballPosY - event.offsetY);
-    if (distX < ballRadius && distY < ballRadius) {
-      count += 1;
-      button.innerHTML = "SCORE: " + count;
-    }
-  };
+ animate();
 
 
-  // RANDOM COLOR CHANGE
-  // const randomColor = () => {
-  //   const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  //   ballColor = "#" + randomColor;
-  //   color.innerHTML = "#" + randomColor;
-  // }
-  // colorButton.addEventListener("click", randomColor);
-  // randomColor();
+ //  CHANGE BALL SPEED ON CLICK
+ canvas.addEventListener("click", function (event) {
+   var distX = Math.abs(ballPosX - event.offsetX);
+   var distY = Math.abs(ballPosY - event.offsetY);
+   var colors = ['#ffb3b3', '#ff8080', '#ff4d4d', '#ff4d4d'];
 
+   if (distX < ballRadius && distY < ballRadius) {
+     ballSpeedX = (Math.floor(Math.random() * 10));
+   }
 
+   if (distX < ballRadius && distY < ballRadius) {
+     ballColor = colors[Math.floor(Math.random() * colors.length)];
+   }
+ })
 
-  blackButton.addEventListener("click", function (event) {
-    ballColor = 'black';
-  });
+ //  SCORE COUNT
+ var button = document.getElementById("scoreCount"),
+   count = 0;
+ canvas.onclick = function () {
+   var distX = Math.abs(ballPosX - event.offsetX);
+   var distY = Math.abs(ballPosY - event.offsetY);
+   if (distX < ballRadius && distY < ballRadius) {
+     count += 1;
+     button.innerHTML = "SCORE: " + count;
+   }
+ };
+
+ colorButton.addEventListener("click", function (event) {
+   ballColor = '#fff';
+ });
