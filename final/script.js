420d205dc419a9be827cb6b9158e2c4ef3df6c9a
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-06-14 09:53:04 -0700

    Final-Add to cart
    Javascript add number to cart icon when button is pressed.

--- final/script.js
+++ final/script.js
@@ -4,4 +4,11 @@ $(document).ready(function () {
         interval: 5000,
         cycle: true
     });
+
+    // ADD TO CART
+    var currentItems = 0;
+       $(".add-to-cart").click(function(){
+           currentItems++;
+           $(".cart-badge").text(currentItems);
+       });
 });
