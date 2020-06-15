7ea2048504429e97bac30127d4764ef3808c1786
Vianey Garcia <garcia_vianey01@student.smc.edu>
2020-06-15 03:57:34 -0700

    Final-cart
    Cart page: image, with text, continue to checkout button
    checkout:  form with validation, button to fill in billing like shipping

--- final/script.js
+++ final/script.js
@@ -7,8 +7,40 @@ $(document).ready(function () {
 
     // ADD TO CART
     var currentItems = 0;
-       $(".add-to-cart").click(function(){
-           currentItems++;
-           $(".cart-badge").text(currentItems);
-       });
+    $(".add-to-cart").click(function () {
+        currentItems++;
+        $(".cart-badge").text(currentItems);
+    });
+
+    //   FORM VALIDATION
+    $("form.needs-validation").submit(function (event) {
+        event.preventDefault();
+
+        $("form.needs-validation").removeClass('needs-validation').addClass('was-validated');
+    })
+
+    // CHECKBOX BILLING SAME
+    $('#check-same').click(function () {
+        if ($('#check-same').is(':checked')) {
+            $('#firstName-billing').val($('#firstName').val());
+            $('#lastName-billing').val($('#lastName').val());
+            $('#email-billing').val($('#email').val());
+            $('#inputAddress-billing').val($('#inputAddress').val());
+            $('#inputAddress2-billing').val($('#inputAddress2').val());
+            $('#inputCountry-billing').val($('#inputCity').val());
+            $('#inputZip-billing').val($('#inputZip').val());
+            var state = $('#inputState option:selected').val();
+            $('#inputState-billing option[value=' + state + ']').attr('selected', 'selected');
+        } else {
+            //Clear on uncheck
+            $('#firstName-billing').val("");
+            $('#lastName-billing').val("");
+            $('#email-billing').val("");
+            $('#inputAddress-billing').val("");
+            $('#inputAddress2-billing').val("");
+            $('#inputCountry-billing').val("");
+            $('#inputZip-billing').val("");
+            $('#inputState-billing option[value=""]').attr('selected', 'selected');
+        };
+    });
 });
