c9f0079e402e597f4e3b3e53b7177f38f8ea1853
Vianey Garcia <garcia_vianey01@student.smc.edu>
2022-03-27 23:32:12 -0700

    Taunt Global Update
    About page: alignment
    Script: Button remove play/pause
    Shop: Add cart button
    Team: added copy

--- midterm/taunt_global/script.js
+++ midterm/taunt_global/script.js
@@ -25,10 +25,8 @@ $(document).ready(function () {
 
         if ($('#carouselControl').hasClass('paused')) {
             $('#myCarousel').carousel('cycle');
-            $('#carouselControl').text('Pause');
         } else {
             $('#myCarousel').carousel('pause');
-            $('#carouselControl').text('Play');
         }
 
         $('#carouselControl').toggleClass('paused');
